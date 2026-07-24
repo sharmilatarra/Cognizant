11d2a5cbe2329e02667d0770ff75a847f3fd7b61
sharmilatarra <sharmilatarra@gmail.com>
2026-07-24 15:02:53 +0530

    Add files via upload

---
+++ DeepSkilling/Module 8 React/Exercise 12/App.js
@@ -0,0 +1,11 @@
+import LoginControl from "./Components/LoginControl";
+
+function App() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <LoginControl />
+    </div>
+  );
+}
+
+export default App;
