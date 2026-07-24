d6d4862e44830a706d004e00fb77e1a7ca20bde5
sharmilatarra <sharmilatarra@gmail.com>
2026-07-24 14:30:19 +0530

    Add files via upload

---
+++ DeepSkilling/Module 8 React/Exercise 11/App.js
@@ -0,0 +1,14 @@
+import EventExamples from "./Components/EventExamples";
+import CurrencyConvertor from "./Components/CurrencyConvertor";
+
+function App() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <EventExamples />
+      <hr />
+      <CurrencyConvertor />
+    </div>
+  );
+}
+
+export default App;
